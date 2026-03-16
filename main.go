d6495c03cff301ec8b411838fc2364840b16b65f
sunlong <ls00330@gdu-tech.com>
2026-03-16 10:19:44 +0800

    测试

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	server := gin.Default()
+	server.GET("/helloworld", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Hello, Gin!",
+			"status":  "success",
+		})
+	})
+
+	server.Run(":8080")
+}
